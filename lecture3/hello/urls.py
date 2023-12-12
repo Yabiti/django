09f9125f9cd2643cea399d4d4099edb1ecc6a95a
Yabiti <yabetseyohannes@gmail.com>
2023-12-12 14:20:50 +0300

    .

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("yabu", views.yabu, name="yabu"),
-    path('David', views.David, name="DAvid")
+    path('David', views.David, name="DAvid"),
 ]
