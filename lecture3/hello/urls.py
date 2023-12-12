592a77de0f71d6dc33d6087baa80f6407bffe99e
Yabiti <yabetseyohannes@gmail.com>
2023-12-12 14:31:03 +0300

    .

--- lecture3/hello/urls.py
+++ lecture3/hello/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.greet),
+    path("<str:name>", views.greet, name="greet"),
     path("yabu", views.yabu, name="yabu"),
     path('David', views.David, name="David"),
 ]
