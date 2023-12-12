ba6f3eb4ad1c9f6b4924e52b5718981ef2d86502
Yabiti <yabetseyohannes@gmail.com>
2023-12-12 15:08:57 +0300

    .

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -10,5 +10,5 @@ def yabu(request):
 
 def David(request):
     return HttpResponse("Hello, David")
-def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+def render(request, "hello/greet.html"):
+    return 
