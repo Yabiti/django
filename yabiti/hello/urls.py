64f07ebc0618bab7b0965586c7bd3b03526db414
Yabiti <yabetseyohannes@gmail.com>
2023-12-15 14:02:22 +0300

    .

--- yabiti/hello/urls.py
+++ yabiti/hello/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
     path("yabiti", views.yabiti, name="yabiti"),
     path("barni", views.barni, name="barni")
 ]
