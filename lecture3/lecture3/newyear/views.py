31cf9e618ad492488eee3e404d1783e5ad3d46ed
Yabiti <yabetseyohannes@gmail.com>
2023-12-15 10:01:22 +0300

    .

--- lecture3/lecture3/newyear/views.py
+++ lecture3/lecture3/newyear/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    datetime = datetime.datetimenow()
-    return render(request, "newyear/index.html")
+    now = datetime.datetimenow()
+    return render(request, "newyear/index.html", {
+        "newyear": now.month == 1 and now.day == 1
+    })
