2451c0775b3d11e5a038bc788f18952a974d414b
Yabiti <yabetseyohannes@gmail.com>
2023-12-15 10:03:34 +0300

    .

--- lecture3/lecture3/newyear/views.py
+++ lecture3/lecture3/newyear/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "newyear/index.html")
+    return render(request, "newyear/index.html", {
+
+    })
